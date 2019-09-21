cce7695c8d90a72fef379d9cf5163b39a6d15b9e
Vladimir Simic <simic0711@gmail.com>
2019-09-21 16:28:56 +0200

    svetsko prvenstvo primer

--- SvetskaPrvenstva/src/svetskaPrvenstva/ui/DrzavaUI.java
+++ SvetskaPrvenstva/src/svetskaPrvenstva/ui/DrzavaUI.java
@@ -3,10 +3,14 @@ package svetskaPrvenstva.ui;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import svetskaPrvenstva.model.Drzava;
 
 public class DrzavaUI {
+	
+	private static Map<String, Drzava> drzave = new HashMap<String, Drzava>();
 	
 	public static void ucitaj() {
 		BufferedReader in =  null;
@@ -16,7 +20,11 @@ public class DrzavaUI {
 			String linija = in.readLine();
 			while (linija != null) {
 				String[] tokeni = linija.split(",");
-
+				
+				drzave.put(tokeni[0], new Drzava(tokeni[0]);
+				
+				// new Drzava();
+				
 				linija = in.readLine();
 			}
 			in.close();
@@ -28,11 +36,16 @@ public class DrzavaUI {
 	}
 
 	public static Drzava getByNaziv(String naziv) {
-		return null;
+		
+		return drzave.get(naziv);
 	}
 	
 	public static void prikazSvihDrzava() {
 
+		for(Drzava d: drzave.values()) {
+			System.out.println(d.getNaziv);
+		}
+		
 	}
 
 	public static void statistika() {
--- SvetskaPrvenstva/src/svetskaPrvenstva/ui/SvetskoPrvenstvoUI.java
+++ SvetskaPrvenstva/src/svetskaPrvenstva/ui/SvetskoPrvenstvoUI.java
@@ -3,10 +3,14 @@ package svetskaPrvenstva.ui;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
 import svetskaPrvenstva.model.SvetskoPrvenstvo;
 
 public class SvetskoPrvenstvoUI {
+	
+	private static Map<Integer, SvetskoPrvenstvo> prvenstva = new HashMap<Integer, SvetskoPrvenstvo>();
 	
 	public static void ucitaj() {
 		BufferedReader in =  null;
@@ -16,7 +20,13 @@ public class SvetskoPrvenstvoUI {
 			String linija = in.readLine();
 			while (linija != null) {
 				String[] tokeni = linija.split(",");
-
+				
+				int godina = Integer.parseInt(tokeni[1]);
+				Drzava domacin = DrzavaUI.getByNaziv(tokeni[3]);
+				Drzava osvajac = DrzavaUI.getByNaziv(tokeni[2]);
+				
+				new SvetskoPrvenstvo(godina, tokeni[0], osvajac, domacin);
+				
 				linija = in.readLine();
 			}
 			in.close();
