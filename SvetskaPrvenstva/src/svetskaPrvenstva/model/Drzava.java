47ad6e899c48165a41c279d2e9400486765bb8ed
vladimir Simic <simic0711@gmail.com>
2019-09-23 00:11:41 +0200

    sv deleted

--- SvetskaPrvenstva/src/svetskaPrvenstva/model/Drzava.java
+++
@@ -1,7 +0,0 @@
-package svetskaPrvenstva.model;
-
-public class Drzava {
-	
-	
-
-}
--- SvetskaPrvenstva/src/svetskaPrvenstva/model/SvetskoPrvenstvo.java
+++
@@ -1,6 +0,0 @@
-package svetskaPrvenstva.model;
-
-public class SvetskoPrvenstvo {
-		//jedna drzava moze da bude osvajac svestko prvenstva
-	//jedna drzava moze da bude domacin svetskih prvenstava
-}
